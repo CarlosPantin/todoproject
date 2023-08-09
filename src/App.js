78e0554d848d3df248adb0e934e5c1f5f45cd515
CarlosPantin~ <carlospantin@live.com.mx>
2023-08-09 15:18:41 +0300

    Changes to be committed: modified:   package-lock.json modified:   package.json modified:   src/App.css modified:   src/App.js deleted:    src/App.test.js deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCircleCheck,faPen,faTrashCan} from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState([
+    {id: 1, title: "Task 1", status: false},
+    {id: 2, title: "Task 2", status: false}
+  ]);
+
+  const [newTask, setNewTask]= useState('');
+  const [updateData, setUpdateData]= useState('');
+  //add task
+  const addTask = () =>{
+
+  }
+  //delete task
+  const deleteTask = (id) =>{
+    
+  }
+  //mark task as done
+  const completeTask = (id) =>{
+    
+  }
+  //cancel update
+  const cancelUpdate = () =>{
+    
+  }
+  //Change task for update
+  const changeTask = (e) =>{
+    
+  }
+  //Update task
+  const updateTask = () =>{
+    
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <br></br>
+      <h2>To List App</h2>
+      <br></br>
+
+      {/* Display Tasks */}
+      {task && task.length ? '' : 'No tasks...'}
+      {task && task 
+      .map( (task,index) => {
+
+      
+        return(
+      <React.Fragment key={task.id}>
+        <div className="col taskBg">
+           <div className={task.status ? 'done': ''}>
+            <span className="taskText">{index+ 1}</span>
+             <span className="taskText">{task.title}</span>
+          </div>
+        </div>
+        
+      </React.Fragment>
+      )
+    })}
     </div>
   );
 }
